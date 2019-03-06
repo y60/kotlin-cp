f4052a7d9ef9d4cc0ffc04427b6f582bf11a6522
Yusuke Hara <yh@g.ecc.u-tokyo.ac.jp>
2019-03-07 00:50:18 +0900

    UnionFindgit

---
+++ UnionFind.kt
@@ -0,0 +1,43 @@
+/**
+ * UnionFind
+ * Each node corresponds to a number (0..`size`-1)
+ */
+class UnionFind(size:Int){
+    private val parents: Array<Int?> =  arrayOfNulls(size)
+    private val sizes: Array<Int> =  Array(size) { i -> 1}
+
+    /**
+     * Returns the root node of `node`.
+     */
+    fun root(node: Int):Int{
+        return if (parents[node] == null){
+            node
+        }else{
+            parents[node]=root(parents[node]!!)
+            parents[node]!!
+        }
+    }
+
+    /**
+     * Unites two groups that contain node `u` and `v`.
+     */
+    fun unite(u:Int,v:Int){
+        var ur = root(u)
+        var vr = root(v)
+        if(sizes[ur]>sizes[vr]){
+            ur = vr .apply{vr=ur}
+        }
+        parents[ur] = vr
+        sizes[vr] += sizes[ur]
+    }
+
+    /**
+     * Returns if node `u` and `v` are in the same group.
+     */
+    fun same(u:Int,v:Int):Boolean = root(u)==root(v)
+
+    /**
+     * Returns the size of the group which contains node `u`.
+     */
+    fun size(u:Int):Int = sizes[root(u)]
+}
